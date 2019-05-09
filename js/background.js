556cd23d5c53e48389f782aa47c1e428929e751c
Voltext <lokin84250@live.fr>
2019-05-09 08:20:11 +0200

    new local version

--- js/background.js
+++ js/background.js
@@ -1,29 +1,32 @@
+/**********************************************************************************************
+* This Chrome extension will send a desktop notification when FuzeIII starts a livestream on Twitch.tv
+*
+* Developed by Wistaro (javascript) and Voltext (HTML/CSS)
+/**********************************************************************************************/
+
 /*Configuration*/
 var api = 'kraken';
 var client_id = 'uewl7eqqjnnukdvhjzqxvieuoxilzs';
-var user = 'fuzeiii'; 
-var timeToCheckLive = '60000'; //every 1min
-var timeToResetNotifs = '3600000'; //every hour
+var user = 'FuzeIII'; 
+var timeToCheckLive = '30000'; //every 30s
+var timeToResetNotifs = '10800000'; //every 3 hours
 /**************************************************/
 
-if(stateNotif == undefined){
-	var stateNotif = "waiting";
-}
+let stateNotif = "waitNotif";
+let notif;
 
 checkStreamFuze(user, client_id, api, false);
-
-var checkFuze = setInterval(function(){
+ 
+var checkFuze = setInterval(function(){ //background task to get the status of the livestream of Fuze
 	checkStreamFuze(user, client_id, api, true);
-
 }, timeToCheckLive );
 
 var resetNotif = setInterval(function(){ //reset notifications every 10h
-	stateNotif = "waiting";
+	stateNotif = "waitNotif";
 }, timeToResetNotifs );
 
 
 function checkStreamFuze(user, client_id, api, notification){
-//background task to get the status of the livestream of Fuze
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://api.twitch.tv/"+api+"/streams/"+user+"?client_id="+client_id, true);
@@ -35,6 +38,7 @@ xhr.onreadystatechange = function(){
 
 		if(data["stream"] == null){
 			//Fuze is not streaming
+			stateNotif = "waitNotif";
 			$('.msgOffline').show();
 			$('#thirdWordStatusLink').html("[HORS LIGNE]");
 			$('#thirdWordStatusLink').css('color', 'red');
@@ -45,36 +49,51 @@ xhr.onreadystatechange = function(){
 			
 		}else{
 			//Fuze is streaming
+			var liveGame = data["stream"]["game"];
+			var liveTitle = data["stream"]["channel"]["status"];
+			var liveViewersCount = data["stream"]["viewers"];
+
+			if(stateNotif != "notifSent"){
+				stateNotif = "ready2sendNotif";	
+			} 
 			$('#msgOffline').hide();
 			$('#thirdWordStatusLink').html("[EN LIVE]");
 			$('#thirdWordStatusLink').css('color', 'green');
-			$('#viewerCount').html(data["stream"]["viewers"]);
-			$('#gamePlaying').html(data["stream"]["game"]);	
-			$('#liveTitle').html(data["stream"]["channel"]["status"]);
+			$('#viewerCount').html(liveViewersCount);
+			$('#gamePlaying').html(liveGame);	
+			$('#liveTitle').html(liveTitle);
 			$('.msgOffline').hide();
-			chrome.browserAction.setIcon({path: "img/logo_green_38.png"});	
+			$('.gameBox').css('visibility', 'visible');
+			chrome.browserAction.setIcon({path: "img/logo_green_38.png"});
+		}	
 			
-			if(stateNotif != "clicked" && notification==true){
-				console.log("[DEBUG - Wistaro] Notification sent to desktop");
-				var notif = new Notification('Live Twitch de FuzeIII!',	{
-										icon : 'img/logo_green_256.png',
-										body : 'FuzeIII est actuellement en live sur Twitch! (clique ici pour arrêter les notifications)'
-					});
+			if(stateNotif == "ready2sendNotif" && notification==true){
+				cleanNotif(notif);
+				notif = sendNotif();
+				stateNotif = "notifSent";
+			}
 
-				notif.onclick = function(event) {
-					  event.preventDefault(); 
-					  window.open('http://twitch.tv/FuzeIII', '_blank');
-					  stateNotif = "clicked";
-					  notif.close();
+				if(typeof notif != "undefined"){
+					notif.onclick = function(event) {
+						  event.preventDefault(); 
+						  window.open('http://twitch.tv/FuzeIII', '_blank');
+						  cleanNotif(notif);
 					}
-				}
-			}										
-			
+				}																				
 		}
 	}
 	xhr.send();
 }
 
-
-
-
+function cleanNotif(notif){
+	if(typeof notif != "undefined"){
+		notif.close();
+	}
+}
+function sendNotif(){
+	return new Notification('Live Twitch de FuzeIII!',	{
+					icon : 'img/logo_green_256.png',
+					title : 'Live Twitch de FuzeIII!', 
+					body : 'FuzeIII est actuellement en live sur Twitch! (clique ici pour accéder au live)'
+	});
+}
